1561db4553b47b52c25b7e71f0fca2bf5e8ff856
PerryThePear <91431688+PerryThePear@users.noreply.github.com>
2023-11-11 17:01:03 -0500

    Library Page
    Library Page skeleton is all set up.

--- react-flask-app/src/pages/library.js
+++ react-flask-app/src/pages/library.js
@@ -1,11 +1,31 @@
+import "../styles/library.css"
+
 import Header from "../components/header";
+import Wordbox from "../components/wordbox";
+
+import { Box, Card, CardContent, Typography } from "@mui/material";
 
 function Library() {
   return(
-    <div>
+    
+    <Box className="content-container">
       <Header/>
-      <h1>LIBRARY</h1>
-    </div>
+      
+      <Box className="main-container">
+        <Box className="flashcard-container">
+          <Card>
+            <CardContent>
+              <Typography>
+                THIS IS WHERE THE FLASH CARD GOES
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
+
+        <Wordbox/>
+
+      </Box>
+    </Box>
   )
 }
 
